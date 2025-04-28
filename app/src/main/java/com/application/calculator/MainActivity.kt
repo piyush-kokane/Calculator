ba14006f1c8893c0a4dd8e2f75c1a42a297159c4
Piyush Kokane <pjkokane21@gmail.com>
2025-04-28 15:41:35 +0530

    Initial commit

---
+++ app/src/main/java/com/application/calculator/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.application.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var Solution: TextView
+    private lateinit var Result: TextView
+    private var currentInput: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+
+        Solution = findViewById(R.id.Solution)
+        Result = findViewById(R.id.Result)
+
+        // Connect number buttons
+        val button0: Button = findViewById(R.id.Btn_0)
+        val button1: Button = findViewById(R.id.Btn_1)
+        val button2: Button = findViewById(R.id.Btn_2)
+        val button3: Button = findViewById(R.id.Btn_3)
+        val button4: Button = findViewById(R.id.Btn_4)
+        val button5: Button = findViewById(R.id.Btn_5)
+        val button6: Button = findViewById(R.id.Btn_6)
+        val button7: Button = findViewById(R.id.Btn_7)
+        val button8: Button = findViewById(R.id.Btn_8)
+        val button9: Button = findViewById(R.id.Btn_9)
+
+        button0.setOnClickListener { appendNumber("0") }
+        button1.setOnClickListener { appendNumber("1") }
+        button2.setOnClickListener { appendNumber("2") }
+        button3.setOnClickListener { appendNumber("3") }
+        button4.setOnClickListener { appendNumber("4") }
+        button5.setOnClickListener { appendNumber("5") }
+        button6.setOnClickListener { appendNumber("6") }
+        button7.setOnClickListener { appendNumber("7") }
+        button8.setOnClickListener { appendNumber("8") }
+        button9.setOnClickListener { appendNumber("9") }
+
+    }
+
+    private fun appendNumber(number: String) {
+        currentInput += number
+        Solution.text = currentInput
+    }
+
+
+
+
+}
